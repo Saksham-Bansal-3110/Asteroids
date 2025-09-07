33ed4bba989811e60f1239d31b2f197916508b0f
Saksham-Bansal-3110 <sakshambansal3110@gmail.com>
2025-09-07 13:35:24 +0000

    Added player class

--- circleshape.py
+++ circleshape.py
@@ -3,7 +3,7 @@ import pygame
 #Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self,x,y,radius):
-        if hasattr(Self,"containers"):
+        if hasattr(self,"containers"):
             super().__init__(self.containers)
         else:
             super().__init__()
